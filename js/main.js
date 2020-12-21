929f4bf72338bffcff1c6e65baeafcedf690284c
Anatoluch <crimeagold@mail.ru>
2020-12-21 21:19:38 +0300

    9th commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
-const toggleMenu = document.querySelector('.toggle-menu');
+const toggleMenu = document.querySelector('.toggle-menu');//Иконка меню "Гамбургер"
+const mobMenu = document.querySelector('.mobile-menu');// Плашка под мобильное меню
+
 toggleMenu.addEventListener('click', function(){
+
    this.classList.toggle('active');
+   mobMenu.classList.toggle('active');
 })
