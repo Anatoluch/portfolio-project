6d274721344b59d61199118a33a3d5a3f90d10f0
Anatoluch <crimeagold@mail.ru>
2021-01-02 13:12:24 +0300

    16th commit

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-const toggleMenu = document.querySelector('.toggle-menu');//Иконка меню "Гамбургер"
+$(document).ready(function () {
+   const toggleMenu = document.querySelector('.toggle-menu');//Иконка меню "Гамбургер"
 const mobMenu = document.querySelector('.mobile-menu');// Плашка под мобильное меню
 const overlay = document.querySelector('#overlay');// overlae
 const bodyEl = document.body;
 
 //Сценарий события клик по "гамбургеру" (появление/исчезание моб. меню, оверлея)
 toggleMenu.addEventListener('click', function(){
-
    this.classList.toggle('active');
    mobMenu.classList.toggle('active');
    overlay.classList.toggle('active');
@@ -24,4 +24,30 @@ overlay.addEventListener('click', function(){
    toggleMenu.classList.remove('active');
    mobMenu.classList.remove('active');
    bodyEl.classList.remove('noscroll');
+});
+   //-фильтрация проектов
+   let containerEl = document.querySelector('#portfolio-projects');
+
+      let mixer = mixitup(containerEl, {
+         classNames: {
+            block: ""
+         }
+      });
+      //Корректировка размера карточек в процессе фильтрации (отмена/возврат класса portfolio-card--big)
+      const filterToggles = document.querySelectorAll('.portfolio-filter button');
+      const portfolioBigCards = document.querySelectorAll('.portfolio-card');
+      for (let i=0; i < filterToggles.length; i++) {
+         filterToggles[i].addEventListener('click', function (){
+            if (i == 0) {
+               for (let j = 0; j < 2; j++) {
+                  portfolioBigCards[j].classList.add('portfolio-card--big')
+               }
+               } else {
+                  for (let j = 0; j < 2; j++) {
+                     portfolioBigCards[j].classList.remove('portfolio-card--big')
+                  }
+            }
+         });
+      }
+
 })
